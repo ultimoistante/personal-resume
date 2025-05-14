8e3a15c80144b16c3993279a7f13ef0fa40382a4
Salvatore Carotenuto <ultimoistante@gmail.com>
2025-05-14 10:01:19 +0200

    replaced lightbox component; upgraded bootstrap to v5.3.6

--- static/js/resume.js
+++ static/js/resume.js
@@ -1,28 +1,34 @@
-(function ($) {
-	"use strict"; // Start of use strict
+const lightbox = GLightbox({
+	touchNavigation: true,
+	loop: true,
+	autoplayVideos: true
+});
 
-	// Smooth scrolling using jQuery easing
-	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html, body').animate({
-					scrollTop: (target.offset().top)
-				}, 1000, "easeInOutExpo");
-				return false;
-			}
-		}
-	});
+// (function ($) {
+// 	"use strict"; // Start of use strict
 
-	// Closes responsive menu when a scroll trigger link is clicked
-	$('.js-scroll-trigger').click(function () {
-		$('.navbar-collapse').collapse('hide');
-	});
+// 	// Smooth scrolling using jQuery easing
+// 	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+// 		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+// 			var target = $(this.hash);
+// 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+// 			if (target.length) {
+// 				$('html, body').animate({
+// 					scrollTop: (target.offset().top)
+// 				}, 1000, "easeInOutExpo");
+// 				return false;
+// 			}
+// 		}
+// 	});
 
-	// Activate scrollspy to add active class to navbar items on scroll
-	$('body').scrollspy({
-		target: '#sideNav'
-	});
+// 	// Closes responsive menu when a scroll trigger link is clicked
+// 	$('.js-scroll-trigger').click(function () {
+// 		$('.navbar-collapse').collapse('hide');
+// 	});
 
-})(jQuery); // End of use strict
+// 	// Activate scrollspy to add active class to navbar items on scroll
+// 	$('body').scrollspy({
+// 		target: '#sideNav'
+// 	});
+
+// })(jQuery); // End of use strict
